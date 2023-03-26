e4266e60a8d5e2a147ec75f6091f1b11fda1c95b
ThiloK1410 <kloos.thilo@gmail.com>
2023-03-26 23:05:13 +0200

    1. Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+class Storage:
+    def __init__(self):
+        pass
+
+    def add_delivery(self, date=None):
+        if date is None:
+
+
+
+
+if __name__ == "__main__":
+    Lager = Storage()
